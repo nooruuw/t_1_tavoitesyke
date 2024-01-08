35482c458b280bd1a3e06914bd446d9036926f70
nooruuw <nooramaria.ylitalo@gmail.com>
2024-01-08 15:52:36 +0200

    t.1 tavoitesyke

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+const [age,setAge] = useState('');
+const [limits,setLimits] = useState('');
+
+
+
+const calculate = () => {
+  const ageNum = parseInt(age)
+  if (!isNaN(ageNum)) {
+  const lowresult = Math.round((220 - ageNum) * 0.65)
+  const highresult = Math.round((220 - ageNum)* 0.85)
+  setLimits(`${lowresult} - ${highresult}`) 
+}
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.field}>Set your age</Text>
+      <TextInput 
+      style={styles.field}
+      keyboardType ='decimal-pad' 
+      value={age}
+      onChangeText={text => setAge(text)}
+      />
+      <Text style={styles.field}>Heart rate limits</Text>
+      <Text style={styles.field}>{limits}</Text>
+      <Button onPress={calculate} title="Calculate"></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +38,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    padding : 8,
   },
+
+field:{
+  marginBottom: 10,
+},
+
 });
